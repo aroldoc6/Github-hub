e52ad1e815f58cf765082527d8bde045024e7708
Aroldo Coelho <aroldo.git@gmail.com>
2022-10-17 17:36:15 -0300

    first commit

---
+++ src/pages/Blog/components/SearchInput/index.tsx
@@ -0,0 +1,35 @@
+import { SearchInputContainer } from "./styles";
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const searchFormSchema = z.object({
+    query: z.string()
+})
+
+type SearchFormInput = z.infer<typeof searchFormSchema>;
+
+interface SearchFormInputProps{
+    postsLength: number;
+    getPosts: (query?: string) => Promise<void>;
+}
+
+export function SearchInput({ postsLength, getPosts }: SearchFormInputProps ){
+    const {register, handleSubmit} = useForm<SearchFormInput>({
+        resolver: zodResolver(searchFormSchema),
+    });
+
+   async function handleSearchPosts(data: SearchFormInput){
+       await getPosts(data.query);
+    }
+
+    return(
+        <SearchInputContainer onSubmit={handleSubmit(handleSearchPosts)}>
+            <header>
+                <h3>Publicações</h3>
+                <span>{postsLength} publicações</span>
+            </header>
+            <input type="text" placeholder="Buscar conteúdo" {...register('query')}/>
+        </SearchInputContainer>
+    )
+}
